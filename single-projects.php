821096935c2d918282eef1868cb47f3d569d6d48
Isakiye Afasha <afashaisakiye@gmail>
2020-10-21 20:26:14 +0300

    Added comments to projects page

--- single-projects.php
+++ single-projects.php
@@ -7,11 +7,23 @@
             <div class="single-box">
                 <div class="single-box-content">
                     <div class="project-post-content">
-                        <?php get_template_part( 'template-parts/header', get_post_format() ); ?>
                         <div class="project-text">
                             <h1><?php the_title() ?></h1>
                             <?php the_content() ?>
+
+
+                            <?php if ( comments_open() ): ?>
+									<span><i class="fa fa-comment"></i><a href="#"><?php  comments_number(  ); ?></a></span>
+									<?php endif;   ?>
+
+
                         </div>
+
+                        <?php 
+									if ( comments_open() ):
+										comments_template();
+									endif; 
+								?>
                     </div>
                 </div>
             </div>
