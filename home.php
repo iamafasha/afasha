82d61c958be614d986d3f6239941f1f8b21df46f
Isakiye Afasha <afashaisakiye@gmail>
2020-10-21 17:59:40 +0300

    added previous and next working buttons on blog page

--- home.php
+++ home.php
@@ -1,24 +1,24 @@
 <? get_header()  ?>
 <!-- content 
-			================================================== -->
+            ================================================== -->
 <div id="content">
     <div class="inner-content">
         <div class="blog-page">
+            <?php  previous_posts_link("Newer Posts") ?>
             <div class="blog-box">
-
                 <?php if ( have_posts() ) : ?>
                 <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+                    <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
                 <?php endwhile; ?>
                 <?php else : ?>
-                <?php get_template_part( 'template-parts/content', 'none' ); ?>
+                    <?php get_template_part( 'template-parts/content', 'none' ); ?>
                 <?php endif; ?>
-
             </div>
-            <a class="blog-page-link" href="#">Older Posts</a>
+            <?php  next_posts_link("Older Posts") ?>
         </div>
     </div>
 </div>
+
 <!-- End content -->
 
 <? get_footer()  ?>
