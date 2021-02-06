b2d758baac283e857598e4f53b50a168b53c18ab
Isakiye Afasha <afashaisakiye@gmail.com>
2021-02-06 21:34:42 +0300

    Fixed #4 Add categories labels  to posts

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -16,7 +16,6 @@
                         </div>
                         <div class="post-content">
                             <h2><a href="<?php the_permalink( ) ?>"><?php the_title(); ?></a></h2>
-                            <p><?php the_excerpt(  ); ?></p>
                         </div>
                         <?php get_template_part( 'template-parts/footer', '' ); ?>
                     </div>
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,14 +1,17 @@
                 <!-- blog-post -->
                 <div class="blog-post image-post">
                     <div class="inner-post">
-                    <?php 
-                                $pattern = '/(width="|height=")([0-9]+)\"/i';
-                                $x= get_the_post_thumbnail();
-                                echo preg_replace($pattern, '', $x);
-                            ?>
+                    <div class="img-container">
+                        <?php 
+                            $pattern = '/(width="|height=")([0-9]+)\"/i';
+                            $x= get_the_post_thumbnail();
+                            echo preg_replace($pattern, '', $x);
+                        ?>
+                       
+                    </div>
                         <div class="post-content">
+                            <div class="tag"><?php $category = get_the_category();echo $category[0]->cat_name;?></div>
                             <h2><a href="<?php the_permalink(  ) ?>"><?php the_title() ?></a></h2>
-                            <p><?php the_excerpt(  ) ?></p>
                         </div>
                         <?php get_template_part( 'template-parts/footer', '' ); ?>
                     </div>
