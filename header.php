2bf9f480104c6e030740a3e3b6fd38f5fadbf73b
Isakiye Afasha <afashaisakiye@gmail.com>
2021-02-20 08:48:34 +0300

    Enabled yoast SEO

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
 <title><?php bloginfo('name');?> <?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta name="description" content="<?php bloginfo('description'); ?>" />
     <link rel="profile" href="https://gmpg.org/xfn/11" />
     <?php if (is_singular() && pings_open(get_queried_object())) : ?>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
