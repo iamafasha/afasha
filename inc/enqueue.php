347d14aa40ce02be9bf53b07df67bf5d5ddd3c0a
Isakiye Afasha <afashaisakiye@gmail.com>
2021-02-20 08:58:41 +0300

    Fixed #24 Set a maximum height for the blog post images on blogs page

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,7 +8,7 @@ function afasha_load_styles_and_scripts($hook)
     wp_enqueue_style( 'magnific_popup', get_template_directory_uri().'/css/magnific-popup.css' , array(), 'all' );
     wp_enqueue_style( 'fontawesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' , array(), 'all' );
     wp_enqueue_style( 'flexslider', get_template_directory_uri().'/css/flexslider.css' , array(),'1.0' , 'all' );
-    wp_enqueue_style( 'style', get_template_directory_uri().'/css/style.css' , array(), '1.4', 'all' );
+    wp_enqueue_style( 'style', get_template_directory_uri().'/css/style.css' , array(), '1.5', 'all' );
     wp_enqueue_style( 'responsive', get_template_directory_uri().'/css/responsive.css' , array(),"1.1" ,'all' );
     wp_enqueue_style( 'google_font', 'https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic' , array(), 'all' );
    //js
