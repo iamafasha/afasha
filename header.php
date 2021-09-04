3af4c3affb6afd35072e140f27735ae918c8fddc
Afasha Isakiye <afasha.isakiye@tmcg.co.ug>
2021-09-05 00:30:20 +0300

    Make mobile button on same level as header

--- header.php
+++ header.php
@@ -8,19 +8,19 @@
 
 <head>
 
-<title><?php bloginfo('name');?> <?php wp_title(); ?></title>
+<title><?php bloginfo('name');?> <?php wp_title();?></title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <?php if( is_single()): ?>
-        <meta name="description" content="<?php bloginfo('description'); ?>" />
-    <?php endif; ?>
+    <meta charset="<?php bloginfo('charset');?>" />
+    <?php if (is_single()): ?>
+        <meta name="description" content="<?php bloginfo('description');?>" />
+    <?php endif;?>
     <link rel="profile" href="https://gmpg.org/xfn/11" />
-    <?php if (is_singular() && pings_open(get_queried_object())) : ?>
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-    <?php endif; ?>
+    <?php if (is_singular() && pings_open(get_queried_object())): ?>
+    <link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+    <?php endif;?>
     <meta charset="utf-8">
     <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
-    <?php wp_head(); ?>
+    <?php wp_head();?>
     <style>
 
     .meter>span {
@@ -29,46 +29,53 @@
     </style>
 </head>
 
-<body data-url="<?php echo admin_url('admin-ajax.php'); ?>" <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
+<body data-url="<?php echo admin_url('admin-ajax.php'); ?>" <?php body_class();?>>
+    <?php wp_body_open();?>
     <!-- Container -->
     <div id="container">
         <!-- Header
-		    ================================================== -->
+            ================================================== -->
         <header>
+            <div class="menu-brand-wrapper" >
+
+            <a class="elemadded responsive-link menu-button" href="#">
+               <i class="fa fa-bars" aria-hidden="true"></i>
+            </a>
+
             <div class="logo-box">
-                <?php if( function_exists( 'the_custom_logo' ) ):  if(has_custom_logo()): ?>
-                <?php  the_custom_logo();  ?>
-                <?php else:  ?>
-                <h1> <?php bloginfo('name'); ?></h1>
-                <?php endif; endif; ?>
+                <?php if(function_exists('the_custom_logo')): if(has_custom_logo( )):?>
+                        <?php the_custom_logo();?>
+                        <?php else: ?>
+                <h1 style="text-transform:uppercase;" > <?php bloginfo('name');?></h1>
+                <?php endif;endif;?>
 
             </div>
-            <a class="elemadded responsive-link" href="#">Menu</a>
+
+            </div>
 
             <div class="menu-box">
                 <?php wp_nav_menu(
-                            array(
-                            'theme_location' => 'main_menu',
-                            'walker'=>new Walker_Main_Menu(),
-                            'container'=>'ul'
-                            )
-                ); ?>
+    array(
+        'theme_location' => 'main_menu',
+        'walker' => new Walker_Main_Menu(),
+        'container' => 'ul',
+    )
+);?>
             </div>
 
             <div class="social-box">
-              
+
               <?php wp_nav_menu(
-                            array(
-                            'theme_location' => 'social_menu',
-                            'walker'=>new Walker_Social_Menu(),
-                            'container'=>'ul',
-                            'menu_class'=>'social-icons'
-                            )
-                 ); ?>
+    array(
+        'theme_location' => 'social_menu',
+        'walker' => new Walker_Social_Menu(),
+        'container' => 'ul',
+        'menu_class' => 'social-icons',
+    )
+);?>
                 <div class="info">
-                    <a href="mailto::<?php echo get_option( 'admin_email', '' ); ?>" class="mail"><?php echo get_option( 'admin_email', '' ); ?><i class="fa fa-envelope-o"></i></a>
-                    <p class="phone"> <a href="tel:<?php echo get_option( 'phone_number', '' ); ?>"><?php echo get_option( 'phone_number', '' ); ?></a><i class="fa fa-phone"></i></p>
+                    <a href="mailto::<?php echo get_option('admin_email', ''); ?>" class="mail"><?php echo get_option('admin_email', ''); ?><i class="fa fa-envelope-o"></i></a>
+                    <p class="phone"> <a href="tel:<?php echo get_option('phone_number', ''); ?>"><?php echo get_option('phone_number', ''); ?></a><i class="fa fa-phone"></i></p>
                 </div>
             </div>
         </header>
