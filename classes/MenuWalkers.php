6fa8e861a6a347b16db9ba0b756d723ea460b555
Afasha Isakiye <afasha.isakiye@tmcg.co.ug>
2021-09-04 10:46:40 +0300

    Upgraded fontawesome

--- classes/MenuWalkers.php
+++ classes/MenuWalkers.php
@@ -10,20 +10,19 @@ class Walker_Social_Menu extends Walker_Nav_menu
     public function start_el(&$output, $item, $depth = 0, $args = array(), $current_object_id = 0)
     {
         //var_dump($item);
-        $output .= '<li><a target="_blank"  class="'.$item->title.'" href="'.$item->url.'" ><i class="fa fa-'.$item->title.'"></i></a>';
+        $output .= '<li><a target="_blank"  class="' . $item->title . '" href="' . $item->url . '" ><i class="fab fa-' . $item->title . '"></i></a>';
     }
 
     /*
-    function end_el(){ // closing li a span
+function end_el(){ // closing li a span
 
-    }
-
-    function end_lvl(){ // closing ul
-
-    }
-    */
 }
 
+function end_lvl(){ // closing ul
+
+}
+ */
+}
 
 class Walker_Main_Menu extends Walker_Nav_menu
 {
@@ -37,20 +36,19 @@ class Walker_Main_Menu extends Walker_Nav_menu
         //var_dump($item);
 
         $is_current_item = '';
-        if(array_search('current-menu-item', $item->classes) != 0)
-        {
+        if (array_search('current-menu-item', $item->classes) != 0) {
             $is_current_item = ' class="active"';
         }
-        $output .= '<li  ><a '.$is_current_item.'  href="'.$item->url.'" ><span>'.$item->title.'</span></a>';
+        $output .= '<li  ><a ' . $is_current_item . '  href="' . $item->url . '" ><span>' . $item->title . '</span></a>';
     }
 
     /*
-    function end_el(){ // closing li a span
+function end_el(){ // closing li a span
 
-    }
+}
 
-    function end_lvl(){ // closing ul
+function end_lvl(){ // closing ul
 
-    }
-    */
+}
+ */
 }
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,26 +1,24 @@
-<?php 
-
+<?php
 
 function afasha_load_styles_and_scripts($hook)
 {
     //css & fonts
-    wp_enqueue_style( 'bootstrapcss', get_template_directory_uri().'/css/bootstrap.css' , array(), 'all' );
-    wp_enqueue_style( 'magnific_popup', get_template_directory_uri().'/css/magnific-popup.css' , array(), 'all' );
-    wp_enqueue_style( 'fontawesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' , array(), 'all' );
-    wp_enqueue_style( 'flexslider', get_template_directory_uri().'/css/flexslider.css' , array(),'1.0' , 'all' );
-    wp_enqueue_style( 'style', get_template_directory_uri().'/css/style.css' , array(), '1.5', 'all' );
-    wp_enqueue_style( 'responsive', get_template_directory_uri().'/css/responsive.css' , array(),"1.1" ,'all' );
-    wp_enqueue_style( 'google_font', 'https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic' , array(), 'all' );
-   //js
-    wp_enqueue_script( 'jquery',  get_template_directory_uri().'/js/jquery.min.js', array(), true );
-    wp_enqueue_script( 'jquery_migrate',  get_template_directory_uri().'/js/jquery.migrate.js', array(), true );
-    wp_enqueue_script( 'jquery_imagesloaded',  get_template_directory_uri().'/js/jquery.imagesloaded.min.js', array(), true );
-    wp_enqueue_script( 'jquery_isotope',  get_template_directory_uri().'/js/jquery.isotope.min.js', array(), true );
-    wp_enqueue_script( 'jquery_magnific_popup',  get_template_directory_uri().'/js/jquery.magnific-popup.min.js', array(), true );
-    wp_enqueue_script( 'jquery_flex_slider',  get_template_directory_uri().'/js/jquery.flexslider.js', array(), true );
-    wp_enqueue_script( 'retina',  get_template_directory_uri().'/js/retina.min.js', array('jquery'), true  );
-    wp_enqueue_script( 'jquery_nicescroll',  get_template_directory_uri().'/js/jquery.nicescroll.min.js', array(), true );
-    wp_enqueue_script( 'script',  get_template_directory_uri().'/js/script.js', array(), true );
+    wp_enqueue_style('bootstrapcss', get_template_directory_uri() . '/css/bootstrap.css', array(), 'all');
+    wp_enqueue_style('magnific_popup', get_template_directory_uri() . '/css/magnific-popup.css', array(), 'all');
+    wp_enqueue_style('flexslider', get_template_directory_uri() . '/css/flexslider.css', array(), '1.0', 'all');
+    wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css', array(), '1.5', 'all');
+    wp_enqueue_style('responsive', get_template_directory_uri() . '/css/responsive.css', array(), "1.1", 'all');
+    wp_enqueue_style('google_font', 'https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic', array(), 'all');
+    //js
+    wp_enqueue_script('font-awesomejs', 'https://kit.fontawesome.com/0672d868fb.js', array(), true);
+    wp_enqueue_script('jquery_migrate', get_template_directory_uri() . '/js/jquery.migrate.js', array(), true);
+    wp_enqueue_script('jquery_imagesloaded', get_template_directory_uri() . '/js/jquery.imagesloaded.min.js', array(), true);
+    wp_enqueue_script('jquery_isotope', get_template_directory_uri() . '/js/jquery.isotope.min.js', array(), true);
+    wp_enqueue_script('jquery_magnific_popup', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', array(), true);
+    wp_enqueue_script('jquery_flex_slider', get_template_directory_uri() . '/js/jquery.flexslider.js', array(), true);
+    wp_enqueue_script('retina', get_template_directory_uri() . '/js/retina.min.js', array('jquery'), true);
+    wp_enqueue_script('jquery_nicescroll', get_template_directory_uri() . '/js/jquery.nicescroll.min.js', array(), true);
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), true);
 }
 
 function afasha_load_admin_styles_and_scripts($hook)
@@ -31,19 +29,17 @@ function afasha_load_admin_styles_and_scripts($hook)
 add_action('wp_enqueue_scripts', 'afasha_load_styles_and_scripts');
 add_action('admin_enqueue_scripts', 'afasha_load_admin_styles_and_scripts');
 
-
-
-function afasha_get_embedded_media( $type = array() ){
-	$content = do_shortcode( apply_filters( 'the_content', get_the_content() ) );
-	$embed = get_media_embedded_in_content( $content, $type );
-	if( in_array( 'audio' , $type) ):
-		$output = str_replace( '?visual=true', '?visual=false', $embed[0] );
-	else:
-		$output = $embed[0];
-	endif;
-	return $output;
+function afasha_get_embedded_media($type = array())
+{
+    $content = do_shortcode(apply_filters('the_content', get_the_content()));
+    $embed = get_media_embedded_in_content($content, $type);
+    if (in_array('audio', $type)):
+        $output = str_replace('?visual=true', '?visual=false', $embed[0]);
+    else:
+        $output = $embed[0];
+    endif;
+    return $output;
 }
-
 
 function sunset_get_bs_slides($attachments)
 {
@@ -54,12 +50,12 @@ function sunset_get_bs_slides($attachments)
 
         $active = ($i == 0 ? ' active' : '');
 
-    $n = ($i == $count ? 0 : $i + 1);
-    $nextImg = wp_get_attachment_thumb_url($attachments[$n]->ID);
-    $p = ($i == 0 ? $count : $i - 1);
-    $prevImg = wp_get_attachment_thumb_url($attachments[$p]->ID);
+        $n = ($i == $count ? 0 : $i + 1);
+        $nextImg = wp_get_attachment_thumb_url($attachments[$n]->ID);
+        $p = ($i == 0 ? $count : $i - 1);
+        $prevImg = wp_get_attachment_thumb_url($attachments[$p]->ID);
 
-    $output[$i] = array(
+        $output[$i] = array(
             'class' => $active,
             'url' => wp_get_attachment_url($attachments[$i]->ID),
             'next_img' => $nextImg,
@@ -72,25 +68,24 @@ function sunset_get_bs_slides($attachments)
     return $output;
 }
 
-
 function sunset_get_attachment($num = 1)
 {
     $output = '';
     if (has_post_thumbnail() && $num == 1):
-        $output = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); else:
+        $output = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));else:
         $attachments = get_posts(array(
             'post_type' => 'attachment',
             'posts_per_page' => $num,
             'post_parent' => get_the_ID(),
         ));
-    if ($attachments && $num == 1):
+        if ($attachments && $num == 1):
             foreach ($attachments as $attachment):
                 $output = wp_get_attachment_url($attachment->ID);
-    endforeach; elseif ($attachments && $num > 1):
+            endforeach;elseif ($attachments && $num > 1):
             $output = $attachments;
-    endif;
+        endif;
 
-    wp_reset_postdata();
+        wp_reset_postdata();
 
     endif;
 
@@ -108,12 +103,12 @@ function sunset_grab_url()
 
 function sunset_grab_quote()
 {
-    $x=array();
-    $post=str_get_html(get_the_content());
-    foreach ( $post->find('blockquote') as $element) {
-        $quote=$element->find('p',0)->plaintext;
-        $author=$element->find('cite',0)->plaintext;
-        array_push($x,["quote"=>$quote,"author"=>$author]);
+    $x = array();
+    $post = str_get_html(get_the_content());
+    foreach ($post->find('blockquote') as $element) {
+        $quote = $element->find('p', 0)->plaintext;
+        $author = $element->find('cite', 0)->plaintext;
+        array_push($x, ["quote" => $quote, "author" => $author]);
     }
     return $x;
 }
